c7f7b10f689fd562622b6b6fc7e2358e60e844ef
iqianxing <qianxingqianli@qq.com>
2018-01-13 22:37:56 +0800

    更新package.json

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,5 +1,4 @@
 var assert = require("chai").assert;
-var should = require("should");
 var fs = require("fs");
 var path = require("path");
 var xml2json = require("xml2json");
@@ -23,10 +22,10 @@ describe("遍历saz解压后的数据", function () {
                     // console.log(sessionJson);
                     sessionJson = JSON.parse(sessionJson);
                     var sessionFlags = sessionJson.Session.SessionFlags.SessionFlag;
-                    var comment = sessionFlags.filter(function(sessionFlag){
-                        return sessionFlag.N =="ui-comments";
+                    var comment = sessionFlags.filter(function (sessionFlag) {
+                        return sessionFlag.N == "ui-comments";
                     })
-                    console.log("      "+comment[0].V)
+                    console.log("      " + comment[0].V)
                     done();
                 })
             });
